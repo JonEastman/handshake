08655b0593c6a3e1bb842fdf58556d43771abe4f
Jon Eastman <eastman.jonathan.d@gmail.com>
2017-08-07 16:10:51 -0500

    embedded example added

--- src/AppBundle/Controller/HelloSignController.php
+++ src/AppBundle/Controller/HelloSignController.php
@@ -13,11 +13,77 @@ class HelloSignController extends Controller
     /**
      * @Route("/hello-sign/integrated", name="hellosign_integrated")
      */
-    public function sendIntegratedRequestAction()
+    public function sendIntegratedRequestAction(Request $request)
     {
+        $handshake = new Handshake();
 
+        $form = $this->createForm(HandshakeType::class, $handshake);
 
-        return $this->render('hellosign/send_request.html.twig');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $response = $this->get('hellosign_manager')->createIntegratedRequest($handshake);
+
+            if ($response) {
+
+                // Typically this would be saved in database until the requested user attempts to sign...
+                $handshake->setRequestId($response->getId());
+
+                // Signature Request ID
+                $signatures = $response->getSignatures();
+
+                // First Signature in collection
+                $signature = $signatures[0];
+
+                $signatureId = $signature->signature_id;
+
+                $this->addFlash('success', 'Yay! You sent a handshake request');
+
+                return $this->redirectToRoute('hellosign_view_embedded', array('id' => $signatureId));
+
+            } else {
+
+                $this->addFlash('error', 'It looks like there was an issue with your request...');
+            }
+        }
+
+        return $this->render('hellosign/send_request.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * This is for example purposes ONLY
+     *
+     * @Route("/hello-sign/integrated/view/{id}", name="hellosign_view_embedded")
+     */
+    public function viewIntegratedRequestAction($id)
+    {
+        // Get Embedded Signature ID
+        $signatureUrl = $this->get('hellosign_manager')->getEmbeddedSignatureUrl($id);
+
+        $clientId = $this->getParameter('hello_sign_client_id');
+
+        $redirectUrl = $this->generateUrl('hellosign_embedded_signed', array(), true);
+
+        return $this->render('hellosign/embedded_request.html.twig', array(
+            'signatureUrl'  => $signatureUrl,
+            'clientId'      => $clientId,
+            'redirectUrl'   => $redirectUrl
+        ));
+    }
+
+    /**
+     * Redirects User back to Homepage with Flash Message
+     *
+     * @Route("/hello-sign/integrated/signed", name="hellosign_embedded_signed")
+     */
+    public function integratedSignedAction()
+    {
+        $this->addFlash('success', 'You successfully signed the Embedded Request');
+
+        return $this->redirectToRoute('homepage');
     }
 
     /**
@@ -33,7 +99,7 @@ class HelloSignController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $requestId = $this->get('handshake_manager')->createWebsiteRequest($handshake);
+            $requestId = $this->get('hellosign_manager')->createWebsiteRequest($handshake);
 
             if ($requestId) {
 
--- src/AppBundle/Utils/HelloSignManager.php
+++ src/AppBundle/Utils/HelloSignManager.php
@@ -4,18 +4,54 @@ namespace AppBundle\Utils;
 
 use AppBundle\Model\Handshake;
 use HelloSign\Client;
+use HelloSign\EmbeddedSignatureRequest;
 use HelloSign\SignatureRequest;
 use Monolog\Logger;
 
 class HelloSignManager
 {
     private $helloSignApiKey;
+    private $helloSignClientId;
     private $logger;
 
-    public function __construct($helloSignApiKey, Logger $logger)
+    public function __construct($helloSignApiKey, $helloSignClientId, Logger $logger)
     {
         $this->helloSignApiKey = $helloSignApiKey;
+        $this->helloSignClientId = $helloSignClientId;
         $this->logger = $logger;
+    }
+
+    /**
+     * Note: This example only allows for 1 signer.
+     *
+     * @param Handshake $handshake
+     * @return mixed
+     */
+    public function createIntegratedRequest(Handshake $handshake)
+    {
+        try {
+
+            $client = new Client('c183c7dab7732e58bb543881f6ed21b31b62855a51ecd26f7b4f91fc514b3983');
+            $request = new SignatureRequest();
+            $request->enableTestMode();
+            $request->setSubject('Embedded Handshake signature request');
+            $request->setMessage($handshake->getMessage());
+            $request->addSigner($handshake->getEmail(), $handshake->getName());
+            $request->addFile('HandShake.pdf');
+
+            $client_id = $this->helloSignClientId;
+            $embedded_request = new EmbeddedSignatureRequest($request, $client_id);
+            $response = $client->createEmbeddedSignatureRequest($embedded_request);
+
+        } catch (\Exception $e) {
+
+            // Log error
+            $this->logger->error('HelloSign Error: '.$e->getMessage());
+
+            return false;
+        }
+
+        return $response;
     }
 
     public function createWebsiteRequest(Handshake $handshake)
@@ -43,5 +79,25 @@ class HelloSignManager
         }
 
         return $requestId;
+    }
+
+    public function getEmbeddedSignatureUrl($signatureId)
+    {
+        try {
+
+            $client = new Client($this->helloSignApiKey);
+            $response = $client->getEmbeddedSignUrl($signatureId);
+
+            $signUrl = $response->getSignUrl();
+
+        } catch (\Exception $e) {
+
+            // Log error
+            $this->logger->error('HelloSign Error: '.$e->getMessage());
+
+            return false;
+        }
+
+        return $signUrl;
     }
 }
